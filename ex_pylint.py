b203537332499f700e3cf2358199ec5c1bc5933f
Gabriel Cuculi <gcuculi@gmail.com>
2025-03-05 14:51:24 -0300

    config: Adding Pylint to project

---
+++ ex_pylint.py
@@ -0,0 +1,8 @@
+print("Olá Mundo")
+
+def minha_funcao():
+    print("Estou na minha função")
+
+# snake_case -> Variáveis e funções em Python (my_variable, my_function)
+# cammelCase -> Não consta: minhaFuncao
+# PascalCase -> Classe em Python
